c577dc88f9abe577981ad5a889bb49eb0aa489e6
unknown <chipurnyukkv@MAIN.SGU.RU>
2021-10-01 11:31:59 +0400

    01.10.2021

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Жду число");
+        Scanner my = new Scanner(System.in);
+        String strocka = my.nextLine();
+
+
+    }
+  public static boolean ticket (String str){
+      if()
+
     }
 }
---
+++ src/test/java/supermegatest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+public class supermegatest {
+    @Test
+    public void test1(){
+        assertTrue
+
+    }
+}
